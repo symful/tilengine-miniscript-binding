a52d4d39a36ce9f393bef958e382384532651208
ardofulian <careday17@gmail.com>
2024-09-07 22:20:15 +0700

    refactor: format

--- Program.cs
+++ Program.cs
@@ -1,21 +1,23 @@
 ﻿using Miniscript;
 using Tilengine;
-using System;
-using System.IO;
-using System.Collections.Generic;
 
-class Program {
-    static void Print(string s, bool lineBreak=true) {
+class Program
+{
+	static void Print(string s, bool lineBreak = true)
+	{
 		if (lineBreak) Console.WriteLine(s);
 		else Console.Write(s);
 	}
 
-	static void ListErrors(Script script) {
-		if (script.errors == null) {
+	static void ListErrors(Script script)
+	{
+		if (script.errors == null)
+		{
 			Print("No errors.");
 			return;
 		}
-		foreach (Miniscript.Error err in script.errors) {
+		foreach (Miniscript.Error err in script.errors)
+		{
 			Print(string.Format("{0} on line {1}: {2}",
 				err.type, err.lineNum, err.description));
 		}
@@ -23,12 +25,13 @@ class Program {
 	}
 
 	static void Test(List<string> sourceLines, int sourceLineNum,
-					 List<string> expectedOutput, int outputLineNum) {
+					 List<string> expectedOutput, int outputLineNum)
+	{
 		if (expectedOutput == null) expectedOutput = [];
-//		Console.WriteLine("TEST (LINE {0}):", sourceLineNum);
-//		Console.WriteLine(string.Join("\n", sourceLines));
-//		Console.WriteLine("EXPECTING (LINE {0}):", outputLineNum);
-//		Console.WriteLine(string.Join("\n", expectedOutput));
+		//		Console.WriteLine("TEST (LINE {0}):", sourceLineNum);
+		//		Console.WriteLine(string.Join("\n", sourceLines));
+		//		Console.WriteLine("EXPECTING (LINE {0}):", outputLineNum);
+		//		Console.WriteLine(string.Join("\n", expectedOutput));
 
 		Interpreter miniscript = new(sourceLines);
 		List<string> actualOutput = [];
@@ -37,33 +40,42 @@ class Program {
 		miniscript.implicitOutput = miniscript.standardOutput;
 		miniscript.RunUntilDone(60, false);
 
-//		Console.WriteLine("ACTUAL OUTPUT:");
-//		Console.WriteLine(string.Join("\n", actualOutput));
+		//		Console.WriteLine("ACTUAL OUTPUT:");
+		//		Console.WriteLine(string.Join("\n", actualOutput));
 
 		int minLen = expectedOutput.Count < actualOutput.Count ? expectedOutput.Count : actualOutput.Count;
-		for (int i = 0; i < minLen; i++) {
-			if (actualOutput[i] != expectedOutput[i]) {
+		for (int i = 0; i < minLen; i++)
+		{
+			if (actualOutput[i] != expectedOutput[i])
+			{
 				Print(string.Format("TEST FAILED AT LINE {0}\n  EXPECTED: {1}\n    ACTUAL: {2}",
 					outputLineNum + i, expectedOutput[i], actualOutput[i]));
 			}
 		}
-		if (expectedOutput.Count > actualOutput.Count) {
+		if (expectedOutput.Count > actualOutput.Count)
+		{
 			Print(string.Format("TEST FAILED: MISSING OUTPUT AT LINE {0}", outputLineNum + actualOutput.Count));
-			for (int i = actualOutput.Count; i < expectedOutput.Count; i++) {
+			for (int i = actualOutput.Count; i < expectedOutput.Count; i++)
+			{
 				Print("  MISSING: " + expectedOutput[i]);
 			}
-		} else if (actualOutput.Count > expectedOutput.Count) {
+		}
+		else if (actualOutput.Count > expectedOutput.Count)
+		{
 			Print(string.Format("TEST FAILED: EXTRA OUTPUT AT LINE {0}", outputLineNum + expectedOutput.Count));
-			for (int i = expectedOutput.Count; i < actualOutput.Count; i++) {
+			for (int i = expectedOutput.Count; i < actualOutput.Count; i++)
+			{
 				Print("  EXTRA: " + actualOutput[i]);
 			}
 		}
 
 	}
 
-	static void RunTestSuite(string path) {
+	static void RunTestSuite(string path)
+	{
 		StreamReader file = new StreamReader(path);
-		if (file == null) {
+		if (file == null)
+		{
 			Print("Unable to read: " + path);
 			return;
 		}
@@ -75,18 +87,27 @@ class Program {
 
 		string? line = file.ReadLine();
 		int lineNum = 1;
-		while (line != null) {
-			if (line.StartsWith("====")) {
+		while (line != null)
+		{
+			if (line.StartsWith("===="))
+			{
 				if (sourceLines != null) Test(sourceLines, testLineNum, expectedOutput!, outputLineNum);
 				sourceLines = null;
 				expectedOutput = null;
-			} else if (line.StartsWith("----")) {
+			}
+			else if (line.StartsWith("----"))
+			{
 				expectedOutput = [];
 				outputLineNum = lineNum + 1;
-			} else if (expectedOutput != null) {
+			}
+			else if (expectedOutput != null)
+			{
 				expectedOutput.Add(line);
-			} else {
-				if (sourceLines == null) {
+			}
+			else
+			{
+				if (sourceLines == null)
+				{
 					sourceLines = [];
 					testLineNum = lineNum;
 				}
@@ -99,9 +120,11 @@ class Program {
 		if (sourceLines != null) Test(sourceLines, testLineNum, expectedOutput!, outputLineNum);
 		Print("\nIntegration tests complete.\n");
 	}
-    static void RunFile(string path, bool dumpTAC=false) {
+	static void RunFile(string path, bool dumpTAC = false)
+	{
 		StreamReader file = new(path);
-		if (file == null) {
+		if (file == null)
+		{
 			Print("Unable to read: " + path);
 			return;
 		}
@@ -109,33 +132,34 @@ class Program {
 		List<string> sourceLines = [];
 		while (!file.EndOfStream) sourceLines.Add(file.ReadLine()!);
 
-        Interpreter miniscript = new(sourceLines)
-        {
-            standardOutput = Print
-        };
-        miniscript.implicitOutput = miniscript.standardOutput;
+		Interpreter miniscript = new(sourceLines)
+		{
+			standardOutput = Print
+		};
+		miniscript.implicitOutput = miniscript.standardOutput;
 		miniscript.Compile();
 
-		if (dumpTAC && miniscript.vm != null) {
+		if (dumpTAC && miniscript.vm != null)
+		{
 			miniscript.vm.DumpTopContext();
 		}
 
 		Inject(miniscript);
-		
-		while (!miniscript.done) {
+
+		while (!miniscript.done)
+		{
 			miniscript.RunUntilDone(int.MaxValue, false);
 		}
-
-        
 	}
 
-    static void Inject(Interpreter interpreter) {
-        ValMap tilengine = new();
+	static void Inject(Interpreter interpreter)
+	{
+		ValMap tilengine = new();
 		ValMap m;
 		Engine engine;
 		Window window;
 
-        Intrinsic f;
+		Intrinsic f;
 
 		{
 			f = Intrinsic.Create("init");
@@ -146,7 +170,8 @@ class Program {
 			f.AddParam("numAnimations");
 			f.AddParam("flag");
 			f.AddParam("loadPath");
-			f.code = (context, partialResult) => {
+			f.code = (context, partialResult) =>
+			{
 				engine = Engine.Init(context.GetLocalInt("hres"), context.GetLocalInt("vres"), context.GetLocalInt("numLayers"), context.GetLocalInt("numSprites"), context.GetLocalInt("numAnimations"));
 				window = Window.Create(null!, (WindowFlags)context.GetLocalInt("flag"));
 
@@ -154,26 +179,28 @@ class Program {
 
 				ValMap r = new();
 				ValMap m;
-        		Intrinsic f;
+				Intrinsic f;
 
 				{
 					m = new();
 					f = Intrinsic.Create("fromFile");
 
 					f.AddParam("path");
-					f.code = (context, partialResult) => {
+					f.code = (context, partialResult) =>
+					{
 						Tilemap map = Tilemap.FromFile(context.GetLocalString("path"), null!);
 						ValMap r = new();
 						ValMap m;
-						Intrinsic f; 
+						Intrinsic f;
 
 						m = new();
-						
+
 						{
 							f = Intrinsic.Create("setLayer");
 
 							f.AddParam("index");
-							f.code = (context, partialResult) => {
+							f.code = (context, partialResult) =>
+							{
 								int idx = context.GetLocalInt("index");
 
 								engine.Layers[idx].SetMap(map);
@@ -184,7 +211,7 @@ class Program {
 							m.SetElem(new ValString(f.name), f.GetFunc());
 							//Print("Injected <Engine>.<Tilemap>.setLayer");
 						}
-							
+
 						return new Intrinsic.Result(m);
 					};
 
@@ -198,7 +225,8 @@ class Program {
 				{
 					f = Intrinsic.Create("process");
 
-					f.code = (context, partialResult) => {
+					f.code = (context, partialResult) =>
+					{
 						bool r = window.Process();
 
 						return r ? Intrinsic.Result.True : Intrinsic.Result.False;
@@ -212,7 +240,8 @@ class Program {
 					f = Intrinsic.Create("drawFrame");
 
 					f.AddParam("frame");
-					f.code = (context, partialResult) => {
+					f.code = (context, partialResult) =>
+					{
 						window.DrawFrame(context.GetLocalInt("frame"));
 
 						return Intrinsic.Result.Null;
@@ -248,10 +277,11 @@ class Program {
 
 		//Print("Injected Tilengine");
 
-        interpreter.SetGlobalValue("Tilengine", tilengine);
-    }
+		interpreter.SetGlobalValue("Tilengine", tilengine);
+	}
 
-	static int Main(string[] args) {
+	static int Main(string[] args)
+	{
 		RunFile(args[0]);
 
 		return 0;
